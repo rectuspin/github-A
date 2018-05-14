9cdbdd1c3245a73937986e8530a13bb5c8ab8e38
Sukhwan Yu <chrisyu94@hanmail.net>
2018-05-14 11:49:43 +0000

    백엔드 넘긴다

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,22 @@
 class PostController < ApplicationController
+    
+    def index
+        @posts=Post.all
+    end
+    
+    def new
+        @post=Post.new
+    end
+    
+    def create
+        @post=Post.new
+        @post.name=params[:name]
+        @post.content=params[:content]
+    end
+    
+  
+    
+    
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  
+  root 'post#index'
+  
+  get 'post/index'
+  
+  get 'post/new'
+  
+  get 'home/create'
+  
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
